afd8dded3e241b5114108de3f3c8d37a2553e4ed
ertrzyiks <mateusz.derks@gmail.com>
2017-01-04 09:55:43 +0100

    Fix linting task

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,8 @@ module.exports = function(grunt) {
         'tasks/*.js',
       ],
       options: {
-        jshintrc: '.jshintrc'
+        jshintrc: '.jshintrc',
+        reporterOutput: ''
       }
     },
 
